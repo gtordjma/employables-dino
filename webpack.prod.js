5ff29abc2e12246915fd579dedd65e3a1a3a83e1
Greg Tordjman <gtordjma@student.42.fr>
2022-05-25 20:53:23 +0200

    dino

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,27 +1,60 @@
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common');
-const TerserPlugin = require('terser-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
-module.exports = merge(common, {
+module.exports = {
   mode: 'production',
-  devtool: false,
-  performance: {
-    maxEntrypointSize: 90000,
-    maxAssetSize: 900000
+  entry: {
+    app: './src/index.js'
+  },
+  devtool: "eval-source-map",
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'build'),
   },
   optimization: {
-    minimizer: [
-      new TerserPlugin({
-        terserOptions: {
-          output: {
-            comments: false
-          }
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendor',
+          chunks: 'all'
         }
-      })
+      }
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        }
+      }
     ]
   },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'build'),
+    compress: true,
+    port: 8080,
+  },
   plugins: [
-    new CleanWebpackPlugin(),
-  ]
-})
+    new webpack.DefinePlugin({
+      'CANVAS_RENDERER': JSON.stringify(true),
+      'WEBGL_RENDERER': JSON.stringify(true)
+    }),
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'assets/**/*'),
+          to: path.resolve(__dirname, 'build')
+        }
+      ],
+    })
+  ],
+};

2ffacdfe9dfa9e86f121219b4448197e60386b18
Greg Tordjman <gtordjma@student.42.fr>
2022-05-25 18:32:26 +0200

    axios

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,24 @@
 import '../styles/globals.css'
 
+export function reportWebVitals(metric) {
+  const url = process.env.NEXT_PUBLIC_AXIOM_INGEST_ENDPOINT;
+
+  if (!url) {
+    return;
+  }
+
+  const body = JSON.stringify({
+    route: window.__NEXT_DATA__.page,
+    ...metric,
+  });
+
+  if (navigator.sendBeacon) {
+    navigator.sendBeacon(url, body);
+  } else {
+    fetch(url, { body, method: 'POST', keepalive: true });
+  }
+}
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
